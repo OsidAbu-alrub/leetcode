b3526810d57da5cbdd8ca1b57816e35269353921
Osid Abu-Alrub <o.abualrub20@gmail.com>
2022-06-01 09:50:15 +0300

    Time: 63 ms (96.95%), Space: 45.3 MB (11.59%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.ts
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.ts
@@ -1,6 +1,7 @@
 function runningSum(nums: number[]): number[] {
     return nums.reduce<number[]>((acc,num,idx) => {
         if(idx === 0) return [num];
-        return [...acc,num + acc[idx-1]];
+        acc[idx] = num + acc[idx-1];
+        return acc;
     },[]);
 };
