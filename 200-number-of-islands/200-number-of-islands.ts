8047a5b4ee5a5ac334b9ecc47fb32ca8e07503f0
Osid Abu-Alrub <o.abualrub20@gmail.com>
2022-07-24 01:06:30 +0300

    Time: 290 ms (8.47%), Space: 52.3 MB (33.58%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.ts
@@ -0,0 +1,31 @@
+function numIslands(grid: string[][]): number {
+    let numberOfIslands = 0;
+    for(let i = 0 ; i < grid.length ; i++){
+        for(let j = 0 ; j < grid[i].length ; j++){
+            if(Boolean(Number(grid[i][j]))){
+                numberOfIslands++;
+                console.log(i, j, numberOfIslands)
+                dfs(i, j, grid);
+            }
+        }
+    }
+    return numberOfIslands;
+};
+
+const directions = [[0, -1], [0, 1], [1, 0], [-1, 0]]
+function dfs(i:number, j:number, grid:string[][]){
+    if(!check(i, j, grid)) return;
+    grid[i][j] = "0";
+    for(const [x, y] of directions)
+        dfs(x+i, y+j, grid);
+}
+    
+function check(i:number, j:number, grid:string[][]){
+    try{
+        if(grid[i][j] === undefined) throw new Error();
+        return Boolean(Number(grid[i][j]));
+    }
+    catch(e){
+        return false;
+    }
+}
