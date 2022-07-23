c7476ba753216ebf817eda0709e70f390caf98e9
Osid Abu-Alrub <o.abualrub20@gmail.com>
2022-07-24 01:18:27 +0300

    Time: 93 ms (61.11%), Space: 44.5 MB (78.57%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.ts
@@ -0,0 +1,9 @@
+function checkIfExist(arr: number[]): boolean {
+    const set = new Set<number>();
+    for(let i = 0 ; i < arr.length - 1 ; i++){
+        for(let j = i + 1 ; j < arr.length ; j++){
+            if(arr[i] === arr[j] * 2 || arr[i] * 2 === arr[j]) return true;
+        }
+    }
+    return false;
+};
