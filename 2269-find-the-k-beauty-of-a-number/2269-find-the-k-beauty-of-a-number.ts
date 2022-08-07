9b10554be2f1769ace4338fcf293cf4aba2b865c
Osid Abu-Alrub <o.abualrub20@gmail.com>
2022-08-08 00:20:45 +0300

    Time: 87 ms (64.10%), Space: 43 MB (84.62%) - LeetHub

--- 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.ts
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.ts
@@ -1,16 +1,21 @@
 function divisorSubstrings(num: number, k: number): number {
-    let divisor = "";
+    let divisor = 0;
     let numberOfBeauties = 0;
-    let stringifiedNum = String(num);
-    
-    for(let windowEnd = 0 ; windowEnd < stringifiedNum.length ; windowEnd++){
-        divisor = `${divisor}${stringifiedNum[windowEnd]}`;
-        if(windowEnd >= k - 1){
-            const divisorAsNumber = Number(divisor);
-            numberOfBeauties = divisorAsNumber !== 0 && num % divisorAsNumber === 0 ? 
+    let power = 1;
+    let n = num;
+    while(n > 0){
+        divisor += (n % 10) * power;
+        k--;
+        if(k > 0)
+            power = power * 10; // increase power by 10 if substring length is not reached
+        else {
+            numberOfBeauties = divisor && num % divisor === 0 ? 
                 numberOfBeauties + 1 : numberOfBeauties
-            divisor = divisor.substring(1);
+            // remove rightmost digit from window 
+            // this is the equivalent of removing window start
+            divisor = Math.floor(divisor / 10); 
         }
+        n = Math.floor(n / 10);
     }
     return numberOfBeauties;
 };
