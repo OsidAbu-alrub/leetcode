41abcd1e92e2d66300398714ac7a075cb85102ec
Osid Abu-Alrub <o.abualrub20@gmail.com>
2022-08-09 00:02:59 +0300

    Time: 197 ms (39.03%), Space: 48.5 MB (61.31%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.ts
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.ts
@@ -1,14 +1,18 @@
 function lengthOfLongestSubstring(s: string): number {
-    const lastOccur = new Array(256);
-    let startIndex = 0;
-    let max = 0;
-    for(let i = 0 ; i < s.length ; i++){
-        const currentChar = s[i].charCodeAt(0);
-        if(lastOccur[currentChar] !== undefined){
-            startIndex = startIndex > lastOccur[currentChar] + 1 ? startIndex : lastOccur[currentChar] + 1;
-        }
-        lastOccur[currentChar] = i;
-        max = max > i - startIndex + 1 ?max : i - startIndex + 1;
-    }
-    return max;
+	const charFrequency = {};
+	let ans = 0;
+	let windowStart = 0;
+	for (let windowEnd = 0; windowEnd < s.length; windowEnd++) {
+		const char = s[windowEnd];
+		if (!(char in charFrequency)) charFrequency[char] = 0;
+		charFrequency[char] += 1;
+		while (charFrequency[char] > 1) {
+			const shrinkChar = s[windowStart];
+			charFrequency[shrinkChar] -= 1;
+			windowStart++;
+		}
+		ans = Math.max(ans, windowEnd - windowStart + 1);
+	}
+  
+	return ans;
 };
