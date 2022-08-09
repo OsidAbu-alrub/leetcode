38462edbf1f643b3f0693334bc9368963b4dd28b
Osid Abu-Alrub <o.abualrub20@gmail.com>
2022-08-10 00:36:34 +0300

    Time: 90 ms (84.62%), Space: 47.9 MB (61.54%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.ts
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.ts
@@ -4,9 +4,14 @@ function longestSubarray(nums: number[]): number {
   let ans = 0;
 
   for(let windowStart = 0, windowEnd = 0 ; windowEnd < nums.length ; windowEnd++){
+    
+    // increase window size
     frequencyHash[nums[windowEnd]] += 1;
     let numberOfRemovals = windowEnd - windowStart + 1 - frequencyHash[1];
 
+    // if the number of removed zeros is greater than one, 
+    // start shrinking window size until number of removed 
+    // zeros in window is less than or equal to one
     while(numberOfRemovals > MAXIMUM_NUMBER_OF_REMOVALS)
     {
       frequencyHash[nums[windowStart]] -= 1;
@@ -15,5 +20,8 @@ function longestSubarray(nums: number[]): number {
     }
     ans = Math.max(ans, windowEnd - windowStart + 1);
   }
+  
+  // we subtract by one because we have to remove at least one element
+  // even if its a one
   return ans - 1;
 };
