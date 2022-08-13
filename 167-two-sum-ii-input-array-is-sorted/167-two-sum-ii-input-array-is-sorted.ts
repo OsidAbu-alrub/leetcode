ce8050755ae86d1e2befecfc27fd91667c1de1c4
Osid Abu-Alrub <o.abualrub20@gmail.com>
2022-08-13 14:22:14 +0300

    Time: 115 ms (45.95%), Space: 44 MB (40.64%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.ts
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.ts
@@ -1,12 +1,13 @@
 function twoSum(numbers: number[], target: number): number[] {
-  for(let start = 0, end = numbers.length - 1 ; start < end ; )
+  const length = numbers.length;
+  for(let start = 0, end = length - 1 ; start < end ; )
   {
     const sum = numbers[start] + numbers[end];
     if(sum === target)
       return [start + 1, end + 1];
-    if(sum < target)
+    else if(sum < target)
       start++;
-    if(sum > target)
+    else
       end--;
   }
 };
