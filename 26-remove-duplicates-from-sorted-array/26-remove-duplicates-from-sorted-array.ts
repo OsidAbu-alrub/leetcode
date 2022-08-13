940814cf6255fe5324b975a255dd71d3b40792b9
Osid Abu-Alrub <o.abualrub20@gmail.com>
2022-08-13 14:45:44 +0300

    Time: 130 ms (53.54%), Space: 44.8 MB (99.10%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.ts
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.ts
@@ -1,11 +1,10 @@
 function removeDuplicates(nums: number[]): number {
-  let ans = 1;
-  for(let left = 0, right = 0 ; right < nums.length ; right++){
+  let left = 0;
+  for(let right = 0 ; right < nums.length ; right++){
     if(nums[left] !== nums[right]){
       left++;
-      ans++;
       nums[left] = nums[right];
     }
   }
-  return ans;
+  return left + 1;
 };
