e2c669238ec26acb734c62f357e1456d3b2b273c
Osid Abu-Alrub <o.abualrub20@gmail.com>
2022-08-23 21:06:16 +0300

    Time: 118 ms (59.04%), Space: 49.3 MB (79.52%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.ts
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.ts
@@ -1,9 +1,17 @@
 function maxScore(cardPoints: number[], k: number): number {
+  
+  // first get the sum from length of array - k to length of array 
   let sum = 0;
   for(let i = cardPoints.length - k ; i < cardPoints.length ; i++)
     sum += cardPoints[i];
+  
+  // set the maxSum as the sum we got previously
   let maxSum = sum;
-  for(let i = 0, j = cardPoints.length - k ; j < cardPoints.length ; i++, j++){
+  
+  // loop k times, removing the values added from previous loop (from end of array)
+  // and adding new values from beginning of array
+  // take the maximum value that you find whilst doing the previous two steps
+  for(let i = 0, j = cardPoints.length - k ; i < k ; i++, j++){
     sum = sum - cardPoints[j] + cardPoints[i];
     maxSum = Math.max(maxSum, sum);
   }
