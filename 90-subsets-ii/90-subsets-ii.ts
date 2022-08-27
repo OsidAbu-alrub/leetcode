108f6a74417b42419159723c418b22038bea15aa
Osid Abu-Alrub <o.abualrub20@gmail.com>
2022-08-27 13:58:52 +0300

    Time: 110 ms (67.20%), Space: 44.4 MB (100.00%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.ts
+++ 90-subsets-ii/90-subsets-ii.ts
@@ -1,18 +1,22 @@
 function subsetsWithDup(nums: number[]): number[][] {
   nums.sort((a, b) => a - b);
   const subsets = [[]];
-  let prevLength = 0;
+  let indexOfFirstNewlyAddedElement = 0;
   for(let i = 0 ; i < nums.length ; i++){
-    let start = 0;
+    let startIndex = 0;
     
     // if we find a duplicate element, 
     // then don't add element to all exisiting subsets
     // but instead add to subsets that were added
     // in the previous step only
     if(nums[i] === nums[i - 1])
-      start = prevLength;
-    prevLength = subsets.length;
-    for(let j = start ; j < prevLength ; j++){
+      startIndex = indexOfFirstNewlyAddedElement;
+    
+    // if our subsets length is 4, then the last element added will be
+    // at index 3, but the next element will be added at index 4, thats
+    // why indexOfFirstNewlyAddedElement is equal to subsets.length
+    indexOfFirstNewlyAddedElement = subsets.length;
+    for(let j = startIndex ; j < indexOfFirstNewlyAddedElement ; j++){
       const set = [...subsets[j]];
       set.push(nums[i]);
       subsets.push(set);
