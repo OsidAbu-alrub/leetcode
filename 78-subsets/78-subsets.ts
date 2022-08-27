526f4d3e130e1e72354cb8c34db41c673b450f43
Osid Abu-Alrub <o.abualrub20@gmail.com>
2022-08-27 13:32:16 +0300

    Time: 158 ms (5.18%), Space: 45.6 MB (25.30%) - LeetHub

--- 78-subsets/78-subsets.ts
+++ 78-subsets/78-subsets.ts
@@ -1,7 +1,13 @@
 function subsets(nums: number[]): number[][] {
   const subsets = [[]];
   for(const num of nums){
+    
+    // store the original length of subsets array
     const lengthOfSubets = subsets.length;
+    
+    // loop over subsets copying over existing sets, 
+    // and adding the new number to the copied set
+    // the adding the copied set to the subsets array
     for(let i = 0 ; i < lengthOfSubets ; i++){
       const set = [...subsets[i]];
       set.push(num);
