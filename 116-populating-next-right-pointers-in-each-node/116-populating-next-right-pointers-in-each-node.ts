04d803f28d9c39b11b26df6f68991e4456395ee4
Osid Abu-Alrub <o.abualrub20@gmail.com>
2022-08-30 20:40:37 +0300

    Time: 157 ms (18.56%), Space: 49.8 MB (79.38%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.ts
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.ts
@@ -1,43 +1,22 @@
-/**
- * Definition for Node.
- * class Node {
- *     val: number
- *     left: Node | null
- *     right: Node | null
- *     next: Node | null
- *     constructor(val?: number, left?: Node, right?: Node, next?: Node) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
-
 function connect(root: Node | null): Node | null {
-    if(!root) return root;
-    let currentLevelQueue = [root];
-    let nextLevelQueue = [];
-    let level = [];
-    while(currentLevelQueue.length){
-        const node = currentLevelQueue.shift();
-        
-        level.push(node);
-        if(node.left) nextLevelQueue.push(node.left);
-        if(node.right) nextLevelQueue.push(node.right);
-        
-        if(!currentLevelQueue.length){
-            pointToRight(level);
-            level = [];
-            [currentLevelQueue, nextLevelQueue] = [nextLevelQueue, currentLevelQueue];
-        }
+  let currentLevel = [root];
+  let nextLevel = [];
+  let prev = null;
+  while(currentLevel.length && root){
+    const node = currentLevel.shift();
+    if(prev){
+      prev.next = node;
+      prev = node;
     }
-    return root;
+    if(!prev)
+      prev = node;
+    if(node.left) nextLevel.push(node.left);
+    if(node.right) nextLevel.push(node.right);
+
+    if(!currentLevel.length){
+      prev = null;
+      [currentLevel, nextLevel] = [nextLevel, currentLevel];
+    }
+  }
+  return root;
 };
-    
-function pointToRight(list: Array<Node>){
-    list.forEach((node, idx, l) => {
-        if(idx === l.length - 1) return;
-        node.next = l[idx + 1]
-    })
-}
