b87d0872b25f07e2838dbb167ad2f1ee4febe4a4
Osid Abu-Alrub <o.abualrub20@gmail.com>
2022-08-30 19:28:13 +0300

    Time: 133 ms (15.69%), Space: 44.1 MB (96.08%) - LeetHub

--- 107-binary-tree-level-order-traversal-ii/107-binary-tree-level-order-traversal-ii.ts
+++ 107-binary-tree-level-order-traversal-ii/107-binary-tree-level-order-traversal-ii.ts
@@ -1,19 +1,5 @@
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
-
 function levelOrderBottom(root: TreeNode | null): number[][] {
-    if(!root) return []
+  if(!root) return []
   let currentLevelQueue = [root];
   let nextLevelQueue = [];
   const answer = []
@@ -27,12 +13,12 @@ function levelOrderBottom(root: TreeNode | null): number[][] {
     if(node.right) nextLevelQueue.push(node.right);
     if(!currentLevelQueue.length)
     {
-        answer.unshift(level);
+        answer.push(level);
         level = [];
         [currentLevelQueue, nextLevelQueue] = [nextLevelQueue, currentLevelQueue]
     }
   }
-    return answer
+    return answer.reverse();
 };
 
 
