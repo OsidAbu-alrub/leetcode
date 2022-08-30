5c1580ff9160873d343ea9eef9c3823485d221ba
Osid Abu-Alrub <o.abualrub20@gmail.com>
2022-08-31 01:44:58 +0300

    Time: 1150 ms (14.58%), Space: 184.1 MB (5.21%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.ts
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.ts
@@ -1,9 +1,7 @@
 function validPath(n: number, edges: number[][], source: number, destination: number): boolean {
   
   // initliaze visited set to not visit same node twice
-  // and queue for BFS
-  const visited = new Set<number>([source]);
-  const queue = [source];
+  const visited = new Set<number>();
   
   // create the graph
   const graph = new Map<number, Array<number>>();
@@ -16,31 +14,13 @@ function validPath(n: number, edges: number[][], source: number, destination: nu
     graph.get(src).push(dest);
     graph.get(dest).push(src);
   }
-  
-  
-  // as long as queue has nodes
-  while(queue.length){
-    
-    // dequeue the first element
-    const current = queue.shift();
-    
-    // if equal to destination, then return true
-    if(current === destination)
-      return true;
-    
-    
-    // if not, then loop over neighbors and add
-    // the unvisited neighbors to my queue
-    for(const neighbor of graph.get(current)){
-      if(!visited.has(neighbor)){
-        visited.add(neighbor);
-        queue.push(neighbor);
-      }
-    }
-  }
-  
-  // if I exit the loop without reaching the destination
-  // then I knwo that it is impossible to reach destination
-  // (disconnected graph) thus return false
-  return false;
+  dfs(source, graph, visited)
+  return visited.has(destination);
 };
+
+function dfs(node: number, graph: Map<number, Array<number>>, visited: Set<number>){
+  visited.add(node);
+  for(const neighbor of (graph.get(node) || []))
+    if(!visited.has(neighbor))
+      dfs(neighbor, graph, visited);  
+}
