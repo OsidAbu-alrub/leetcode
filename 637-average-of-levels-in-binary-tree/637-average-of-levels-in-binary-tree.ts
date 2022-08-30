e271dd2b26a598021be23783e8a09f1f017b071c
Osid Abu-Alrub <o.abualrub20@gmail.com>
2022-08-30 19:47:16 +0300

    Time: 175 ms (10.00%), Space: 49.3 MB (38.33%) - LeetHub

--- 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.ts
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.ts
@@ -1,28 +1,25 @@
 function averageOfLevels(root: TreeNode | null): number[] {
-    let currentLevel = [root];
-    let nextLevel = [];
-    let sumAtLevel = 0;
-    let lengthAtLevel = 0;
-    const averages = [];
-    while(currentLevel.length){
-        const current = currentLevel.shift();
-        
-        sumAtLevel = (sumAtLevel + current.val) % Number.MAX_SAFE_INTEGER;
-        lengthAtLevel += 1;
-        
-        if(!currentLevel.length)
-            averages.push(average(sumAtLevel, lengthAtLevel))
-        
-        if(current.left) nextLevel.push(current.left);
-        if(current.right) nextLevel.push(current.right);
-        
-        if(!currentLevel.length){
-            sumAtLevel = 0;
-            lengthAtLevel = 0;
-            [currentLevel, nextLevel] = [nextLevel, currentLevel];
-        }
+  let currentLevel: Array<any> = [root];
+  let nextLevel: Array<any> = [];
+  let sumAtLevel = 0;
+  let numberOfNodesAtLevel = 0; 
+  const averages = [];
+  while(currentLevel.length){
+    const current = currentLevel.shift();
+    sumAtLevel = (sumAtLevel + current.val) % Number.MAX_SAFE_INTEGER;
+    numberOfNodesAtLevel++;
+
+    if(current.left) nextLevel.push(current.left);
+    if(current.right) nextLevel.push(current.right);
+
+    if(!currentLevel.length){
+      averages.push(average(sumAtLevel, numberOfNodesAtLevel));
+      sumAtLevel = 0;
+      numberOfNodesAtLevel = 0;
+      [currentLevel, nextLevel] = [nextLevel, currentLevel];
     }
-    return averages;
+  }
+  return averages;
 };
     
 function average(sum:number, quantity:number){
