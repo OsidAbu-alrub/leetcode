b0a6e955e67a8bce41e16f77bad34f38dee50fea
Osid Abu-Alrub <o.abualrub20@gmail.com>
2022-08-31 16:31:35 +0300

    Time: 88 ms (81.89%), Space: 44.9 MB (75.58%) - LeetHub

--- 202-happy-number/202-happy-number.ts
+++ 202-happy-number/202-happy-number.ts
@@ -1,11 +1,27 @@
 function isHappy(n: number): boolean {
+  
+  // we use this set to check if we've calcualted 
+  // the sum of sqaures before or not
   const seen = new Set<number>();
   let sumOfSquares = n;
+  
+  // as long as we didn't calculate the same sum twice
   while(!seen.has(sumOfSquares)){
+    
+    // add it to seen set
     seen.add(sumOfSquares);
+    
+    // calculate the new sum of sqaures
     sumOfSquares = calculateSumOfSquares(sumOfSquares);
+    
+    // if it equals to zero, then return true (happy number)
     if(sumOfSquares === 1) return true; 
   }
+  
+  // if we enter an infinite loop
+  // AKA we calculated the same sum of squares
+  // then we don't have a happy number
+  // return false
   return false;
 };
   
