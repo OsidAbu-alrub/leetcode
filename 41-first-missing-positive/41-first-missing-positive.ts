273822985ac0cca2ee1cb037df0de6ec57825061
Osid Abu-Alrub <o.abualrub20@gmail.com>
2022-09-01 22:21:51 +0300

    Time: 128 ms (81.16%), Space: 49.1 MB (91.30%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.ts
+++ 41-first-missing-positive/41-first-missing-positive.ts
@@ -1,5 +1,8 @@
 function firstMissingPositive(nums: number[]): number {
   let i = 0;
+  
+  // cyclic sort the array
+  // ignore any values that are less 1 and more than length of array
   while(i < nums.length){
     let j = nums[i] - 1;
     if(nums[i] > 0 && nums[i] <= nums.length && nums[j] !== nums[i])
@@ -8,8 +11,15 @@ function firstMissingPositive(nums: number[]): number {
       i++;
   }
   
+  // find first element that doesn't match
+  // its index + 1
+  // return it
   for(let i = 0 ; i < nums.length ; i++)
     if(nums[i] !== i + 1)
       return i + 1;
+  
+  // if all elements match their index + 1
+  // then the smallest positive missing value
+  // from this array is the array length + 1
   return nums.length + 1;
 };
