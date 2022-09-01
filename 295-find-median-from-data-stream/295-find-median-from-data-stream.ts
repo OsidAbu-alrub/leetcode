1d4c929251612ebf344e00f18f682003c6597d9e
Osid Abu-Alrub <o.abualrub20@gmail.com>
2022-09-02 00:23:46 +0300

    Time: 630 ms (93.33%), Space: 74.5 MB (99.05%) - LeetHub

---
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.ts
@@ -0,0 +1,47 @@
+class MedianFinder {
+  minHeap;
+  maxHeap;
+  constructor() {
+    this.minHeap = new MinPriorityQueue({ priority: this.comparator }); 
+    this.maxHeap = new MaxPriorityQueue({ priority: this.comparator }); 
+  }
+
+  addNum(num: number): void {
+    const maxHeap = this.maxHeap;
+    const minHeap = this.minHeap;
+    
+    if(maxHeap.isEmpty() || maxHeap.front().element >= num)
+      maxHeap.enqueue(num);
+    else
+      minHeap.enqueue(num);
+    
+    // our case must always be 
+    // either max heap has one more element than min heap
+    // min heap and max heap have same number of elements
+    if(maxHeap.size() > minHeap.size() + 1)
+      minHeap.enqueue(maxHeap.dequeue().element);
+    else if(maxHeap.size() < minHeap.size())
+      maxHeap.enqueue(minHeap.dequeue().element);
+  }
+
+  findMedian(): number {
+    const maxHeap = this.maxHeap;
+    const minHeap = this.minHeap;
+    
+    if((minHeap.size() + maxHeap.size()) % 2 === 0) return Number(((minHeap.front().element + maxHeap.front().element) / 2).toFixed(5));
+    
+    // because max heap will always have one more element (two cases above)
+    return maxHeap.front().element
+  }
+
+  comparator(value: number):number {
+    return value;
+  }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * var obj = new MedianFinder()
+ * obj.addNum(num)
+ * var param_2 = obj.findMedian()
+ */
