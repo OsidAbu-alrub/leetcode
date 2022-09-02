3287d7c57c76a89f188e6b14aa4de75ba7b80953
Osid Abu-Alrub <o.abualrub20@gmail.com>
2022-09-03 00:41:39 +0300

    Time: 346 ms (50.00%), Space: 56.5 MB (50.00%) - LeetHub

---
+++ 320-generalized-abbreviation/320-generalized-abbreviation.ts
@@ -0,0 +1,40 @@
+function generateAbbreviations(word: string): string[] {
+  const result = [];
+  dfs([...word], result, 0);
+  return result;
+};
+
+function dfs(word:string[], result: string[], index: number) {
+  if(index === word.length){
+    result.push(getValidPermutation(word));
+    return;
+  }
+  
+  const char = word[index];
+  word[index] = "1";
+  dfs(word, result, index + 1);
+  word[index] = char;
+  dfs(word, result, index + 1);
+}
+
+function getValidPermutation(word:string[]){
+  const wordCopy = [...word];
+  const lengthOfWordCopy = wordCopy.length;
+  for(let i = 0 ; i < lengthOfWordCopy ; i++){
+    const currentChar = Number(wordCopy[i]);
+    const previousChar = Number(wordCopy[i - 1]);
+    
+    // if both chars are numbers
+    if(!isNaN(currentChar) && !isNaN(previousChar)){
+      // remove previous digit
+      wordCopy[i - 1] = "";
+      
+      // replace current digit with sum of two digits
+      wordCopy[i] = String(currentChar + previousChar);
+    }
+  }
+  
+  // remove empty string from word copy
+  // and convert the wordCopy array to a string
+  return wordCopy.filter(Boolean).join("");
+}
