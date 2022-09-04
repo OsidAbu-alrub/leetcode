0df6937a2568dcd2f18a61e17223fd0c3fee4a1f
Osid Abu-Alrub <o.abualrub20@gmail.com>
2022-09-04 18:37:46 +0300

    Time: 111 ms (65.64%), Space: 45 MB (90.80%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.ts
+++ 547-number-of-provinces/547-number-of-provinces.ts
@@ -1,11 +1,17 @@
+// This question can be rephrased as:
+// How many connected graphs we have
 function findCircleNum(isConnected: number[][]): number {
   let numberOfProvinces = 0;
   const visited = new Set<number>();
-  for(let i = 0 ; i < isConnected.length ; i++)
+  for(let i = 0 ; i < isConnected.length ; i++){
+    
+    // if I find a node that I haven't visited before (new graph)
+    // then visit all its neighbors (flood fill)
     if(!visited.has(i)){
       numberOfProvinces++;
       dfs(i, isConnected, visited);
     }
+  }
   
   return numberOfProvinces;
 };
