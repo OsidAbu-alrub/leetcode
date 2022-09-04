78c706734c59542055900f48a0d4d1cb68de4e84
Osid Abu-Alrub <o.abualrub20@gmail.com>
2022-09-04 18:14:21 +0300

    Time: 160 ms (28.57%), Space: 48.3 MB (42.86%) - LeetHub

--- 1474-delete-n-nodes-after-m-nodes-of-a-linked-list/1474-delete-n-nodes-after-m-nodes-of-a-linked-list.ts
+++ 1474-delete-n-nodes-after-m-nodes-of-a-linked-list/1474-delete-n-nodes-after-m-nodes-of-a-linked-list.ts
@@ -12,18 +12,29 @@
 
 function deleteNodes(head: ListNode | null, m: number, n: number): ListNode | null {
   let current = head;
+  
+  // as long as we have a current
   while(current){
+    
+    // have these two variables to track how
+    // many nodes we've deleted and how many nodes we've skipped
     let skipped = 0;
     let deleted = 0;
+    
+    // keep skipping until you reach a node
+    // before the first node that needs to be deleted
     while(current && skipped < m - 1){
       current = current.next;
       skipped++;
     }
+    
+    // delete n nodes
     while(current && current.next && deleted < n){
       current.next = current.next.next;
       deleted++;
     }
     
+    // move to next node
     if(current)
       current = current.next;
   }
