a90eef0b6453efc41f97e1cee98d40171468a4f9
Osid Abu-Alrub <o.abualrub20@gmail.com>
2022-09-04 21:24:17 +0300

    Time: 165 ms (5.45%), Space: 47.6 MB (12.73%) - LeetHub

---
+++ 657-robot-return-to-origin/657-robot-return-to-origin.ts
@@ -0,0 +1,14 @@
+const MOVE_TO_NUMBER_MAP = {
+  "U": [-1, 0],
+  "D": [1, 0],
+  "L": [0, 1],
+  "R": [0, -1],
+  
+}
+
+function judgeCircle(moves: string): boolean {
+  const coordinates = [0, 0];
+  for(const char of moves)
+    [coordinates[0], coordinates[1]] = [coordinates[0] + MOVE_TO_NUMBER_MAP[char][0], coordinates[1] + MOVE_TO_NUMBER_MAP[char][1]];
+  return coordinates[0] === 0 && coordinates[1] === 0;
+};
