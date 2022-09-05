2d3297136c9ef6aa9f6d9a9fe918cf80da5a760e
Osid Abu-Alrub <o.abualrub20@gmail.com>
2022-09-05 15:59:13 +0300

    Time: 188 ms (56.70%), Space: 43.5 MB (97.94%) - LeetHub

---
+++ 37-sudoku-solver/37-sudoku-solver.ts
@@ -0,0 +1,47 @@
+const EMPTY_CELL = ".";
+const NUMBER_OF_ROWS = 9;
+const NUMBER_OF_COLUMNS = 9;
+const SUB_BOARD_WIDTH = 3;
+const SUB_BOARD_HEIGHT = 3;
+
+function solveSudoku(board: string[][]): void {
+  solve(board);
+};
+
+function solve(board: string[][]) : boolean{
+  for(let i = 0 ; i < NUMBER_OF_ROWS ; i++){
+    for(let j = 0 ; j < NUMBER_OF_COLUMNS ; j++){
+      if(board[i][j] === "."){
+        for(let val = 1 ; val <= 9 ; val++) {
+          if(check(i, j, String(val), board)) {
+            board[i][j] = String(val)
+            if(solve(board))
+              return true;  
+            board[i][j] = ".";
+          }
+        }
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+function check(row: number, column:number, value: string, board: string[][]){
+  for(let i = 0 ; i < NUMBER_OF_COLUMNS ; i++)
+    if(board[row][i] === value)
+      return false;
+  
+  for(let i = 0 ; i < NUMBER_OF_ROWS ; i++)
+    if(board[i][column] === value)
+      return false;
+  
+  const subBoardRows = Math.floor(row / SUB_BOARD_HEIGHT) * SUB_BOARD_HEIGHT;
+  const subBoardColumns = Math.floor(column / SUB_BOARD_WIDTH) * SUB_BOARD_WIDTH;
+  
+  for(let i = 0 ; i < SUB_BOARD_HEIGHT ; i++)
+    for(let j = 0 ; j < SUB_BOARD_WIDTH ; j++)
+      if(board[i + subBoardRows][j + subBoardColumns] === value)
+        return false;
+  return true;
+}
