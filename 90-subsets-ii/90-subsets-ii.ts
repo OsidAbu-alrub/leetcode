dd099644e2efa3bc10ffeeb67342e8560099ece6
Osid Abu-Alrub <o.abualrub20@gmail.com>
2022-09-05 16:58:21 +0300

    Time: 84 ms (89.26%), Space: 45.5 MB (52.89%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.ts
+++ 90-subsets-ii/90-subsets-ii.ts
@@ -1,26 +1,21 @@
 function subsetsWithDup(nums: number[]): number[][] {
   nums.sort((a, b) => a - b);
-  const subsets = [[]];
-  let indexOfFirstNewlyAddedElement = 0;
-  for(let i = 0 ; i < nums.length ; i++){
-    let startIndex = 0;
-    
-    // if we find a duplicate element, 
-    // then don't add element to all exisiting subsets
-    // but instead add to subsets that were added
-    // in the previous step only
-    if(nums[i] === nums[i - 1])
-      startIndex = indexOfFirstNewlyAddedElement;
-    
-    // if our subsets length is 4, then the last element added will be
-    // at index 3, but the next element will be added at index 4, thats
-    // why indexOfFirstNewlyAddedElement is equal to subsets.length
-    indexOfFirstNewlyAddedElement = subsets.length;
-    for(let j = startIndex ; j < indexOfFirstNewlyAddedElement ; j++){
-      const set = [...subsets[j]];
-      set.push(nums[i]);
-      subsets.push(set);
-    }
-  }
+  const subsets = [];
+  dfs(nums, 0, [], subsets);
   return subsets;
 };
+
+function dfs(nums, index, currentPath, subsets){
+  if(index === nums.length){
+    subsets.push([...currentPath])
+    return;
+  }
+  
+  currentPath.push(nums[index]);
+  dfs(nums, index + 1, currentPath, subsets);
+  currentPath.pop();
+  
+  while(nums[index] === nums[index + 1])
+    index++;
+  dfs(nums, index + 1, currentPath, subsets);
+}
