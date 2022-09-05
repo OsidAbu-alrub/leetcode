9d4198ba345d919a2b8edf5c8f9c24d54eddebcc
Osid Abu-Alrub <o.abualrub20@gmail.com>
2022-09-05 16:08:57 +0300

    Time: 129 ms (54.69%), Space: 46.9 MB (54.87%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.ts
+++ 36-valid-sudoku/36-valid-sudoku.ts
@@ -14,21 +14,30 @@ function isValidSudoku(board: string[][]): boolean {
     const box =  Math.floor(row / 3) * 3 + Math.floor(column / 3);
     const cell = Number(board[row][column]);
     
-    // if empty cell
+    // if empty cell, skip
     if(isNaN(cell)) continue;
     
+    // initialize sets if not initialized for
+    // row, column, or box
     if(!rows.has(row)) rows.set(row, new Set<number>());
     if(!columns.has(column)) columns.set(column, new Set<number>());
     if(!boxes.has(box)) boxes.set(box, new Set<number>());
     
+    // if the cell value was seen before, this means
+    // this is an invalid sudoku, return false
     if(rows.get(row).has(cell)) return false;
     if(columns.get(column).has(cell)) return false;
     if(boxes.get(box).has(cell)) return false;
     
+    // if we haven't seen the cell before, add it to
+    // row, column, and box
     rows.get(row).add(cell)
     columns.get(column).add(cell)
     boxes.get(box).add(cell)
   }
   
+  
+  // if we manage to exit the loop without breaking
+  // sudoku rules, then this is a valid sudoku
   return true
 };
