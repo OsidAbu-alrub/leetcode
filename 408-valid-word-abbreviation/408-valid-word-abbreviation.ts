ed04a90ff4a6aa110a289bffb0fe9b4d81a2ab92
Osid Abu-Alrub <o.abualrub20@gmail.com>
2022-09-07 20:22:18 +0300

    Time: 73 ms (93.33%), Space: 43.4 MB (93.33%) - LeetHub

--- 408-valid-word-abbreviation/408-valid-word-abbreviation.ts
+++ 408-valid-word-abbreviation/408-valid-word-abbreviation.ts
@@ -2,7 +2,6 @@ function validWordAbbreviation(word: string, abbr: string): boolean {
   if(abbr.length > word.length) return false;
   let i = 0;
   let j = 0;
-  let num = "";
   while(i < word.length && j < abbr.length){
     if(word[i] === abbr[j]){
       i++;
@@ -15,16 +14,25 @@ function validWordAbbreviation(word: string, abbr: string): boolean {
     {
       numberOfSkips = (numberOfSkips * 10) + Number(abbr[j]);
       
+      // Abbreviations with leading zeros are invalid
       if(numberOfSkips === 0)
         return false;
       
       j++;
     }
     
+    
+    // skip "numberOfSkips" chars from word
     i = numberOfSkips + i;
     
+    // if the current char in word (after the skips)
+    // doesn't equal the char in abbr
+    // then this is an invalid abbrevation
     if(word[i] !== abbr[j])
       return false;
   }
+  
+  // I need to make sure that I reached the end of both strings
+  // to ensure this is a valid abbrevation
   return i === word.length && j === abbr.length;
 };
