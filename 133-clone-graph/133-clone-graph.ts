a94349c382a95669d41026445e31808c52e94c57
Osid Abu-Alrub <o.abualrub20@gmail.com>
2022-09-08 11:53:13 +0300

    Time: 84 ms (78.83%), Space: 45 MB (39.09%) - LeetHub

--- 133-clone-graph/133-clone-graph.ts
+++ 133-clone-graph/133-clone-graph.ts
@@ -1,31 +1,27 @@
-class GraphNode {
-     val: number
-     neighbors: GraphNode[]
-     constructor(val?: number, neighbors?: GraphNode[]) {
-         this.val = (val===undefined ? 0 : val)
-         this.neighbors = (neighbors===undefined ? [] : neighbors)
-    }
-}
+// class GraphNode {
+//      val: number
+//      neighbors: GraphNode[]
+//      constructor(val?: number, neighbors?: GraphNode[]) {
+//          this.val = (val===undefined ? 0 : val)
+//          this.neighbors = (neighbors===undefined ? [] : neighbors)
+//     }
+// }
 
 
-function cloneGraph(node: GraphNode | null): GraphNode | null {
+function cloneGraph(node: Node | null): Node | null {
     if(!node) return null;
-    const clone = new GraphNode(node.val);
-    const visited = new Map<number, GraphNode>();
-    dfs(node, clone, visited);
-    return clone;
+    const visited = new Map<Node, Node>();
+    return dfs(node, visited);
 };
 
-function dfs(node: GraphNode | null, clone: GraphNode, visited: Map<number, GraphNode>) {
-    if(!node) return;
-    visited.set(clone.val, clone);
-    for(const neighbor of node.neighbors){
-        if(visited.has(neighbor.val))
-            clone.neighbors.push(visited.get(neighbor.val));
-        else{
-            const clonedNeighbor = new GraphNode(neighbor.val);
-            clone.neighbors.push(clonedNeighbor);
-            dfs(neighbor, clonedNeighbor, visited)
-        }
-    }
+function dfs(node: Node | null, visited: Map<Node, Node>) {
+  const copy = new Node();
+  copy.val = node.val;
+  visited.set(node, copy);
+  for(const neighbor of node.neighbors)
+    if(visited.has(neighbor))
+      copy.neighbors.push(visited.get(neighbor));
+    else
+      copy.neighbors.push(dfs(neighbor, visited));
+  return copy;
 }
