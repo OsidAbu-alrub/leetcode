1872c3488d6ad740df2d09ecdf9348f2f4df0279
Osid Abu-Alrub <o.abualrub20@gmail.com>
2022-09-19 14:16:39 +0300

    Time: 117 ms (19.41%), Space: 44.4 MB (62.32%) - LeetHub

--- 100-same-tree/100-same-tree.ts
+++ 100-same-tree/100-same-tree.ts
@@ -12,11 +12,29 @@
  * }
  */
 
-function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
-    return sameLevel(p,q);
+function isSameTree(root1: TreeNode | null, root2: TreeNode | null): boolean {
+  if(root1 && !root2 || !root1 && root2) return false;
+  
+  const queueRoot1 = [root1];
+  const queueRoot2 = [root2]; 
+  while(queueRoot1.length && queueRoot2.length && root1 && root2){
+    const node1 = queueRoot1.shift();
+    const node2 = queueRoot2.shift();
+    
+    if(node1.val !== node2.val) return false;
+    if(node1.left && !node2.left || !node1.left && node2.left) return false;
+    if(node1.right && !node2.right || !node1.right && node2.right) return false;
+    
+    if(node1.left) {
+      queueRoot1.push(node1.left);
+      queueRoot2.push(node2.left);
+    }
+    
+    if(node1.right) {
+      queueRoot1.push(node1.right);
+      queueRoot2.push(node2.right);
+    }
+  }
+  
+  return true;
 };
-
-function sameLevel(p: TreeNode | null, q: TreeNode | null){
-    if(!q || !p) return q === p;
-    return p.val === q.val && sameLevel(p.left,q.left) && sameLevel(p.right,q.right);
-}
