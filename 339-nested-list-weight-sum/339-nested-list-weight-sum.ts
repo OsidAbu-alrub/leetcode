aa5436b1514ef93aee859652ad4ae4365ad50b81
Osid Abu-Alrub <o.abualrub20@gmail.com>
2022-09-20 23:54:14 +0300

    Time: 113 ms (21.05%), Space: 44.8 MB (57.89%) - LeetHub

---
+++ 339-nested-list-weight-sum/339-nested-list-weight-sum.ts
@@ -0,0 +1,55 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *     If value is provided, then it holds a single integer
+ *     Otherwise it holds an empty nested list
+ *     constructor(value?: number) {
+ *         ...
+ *     };
+ *
+ *     Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     isInteger(): boolean {
+ *         ...
+ *     };
+ *
+ *     Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     Return null if this NestedInteger holds a nested list
+ *     getInteger(): number | null {
+ *         ...
+ *     };
+ *
+ *     Set this NestedInteger to hold a single integer equal to value.
+ *     setInteger(value: number) {
+ *         ...
+ *     };
+ *
+ *     Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+ *     add(elem: NestedInteger) {
+ *         ...
+ *     };
+ *
+ *     Return the nested list that this NestedInteger holds,
+ *     or an empty list if this NestedInteger holds a single integer
+ *     getList(): NestedInteger[] {
+ *         ...
+ *     };
+ * };
+ */
+
+function depthSum(nestedList: NestedInteger[]): number {
+  return helper(nestedList, 1);
+};
+
+function helper(nestedList: NestedInteger[], depth: number){
+  let sum = 0;
+  for(const element of nestedList){
+    if(element.isInteger()){
+      sum += element.getInteger() * depth;
+    }
+    else {
+      sum += helper(element.getList(), depth + 1);
+    }
+  }
+  return sum;
+}
