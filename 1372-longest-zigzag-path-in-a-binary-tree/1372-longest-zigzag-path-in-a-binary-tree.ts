0b2549759d96fff7478bddf8b38a35acf8bbbeb3
Osid Abu-Alrub <o.abualrub20@gmail.com>
2022-09-20 23:46:42 +0300

    Time: 344 ms (33.33%), Space: 81.5 MB (83.33%) - LeetHub

--- 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.ts
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.ts
@@ -13,11 +13,25 @@
  */
 
 function longestZigZag(root: TreeNode | null): number {
-  if(!root.left && !root.right) return 0;
   return Math.max(dfs(root.left, true, 1), dfs(root.right, false, 1));
 };
 
 function dfs(root: TreeNode | null, isLeftChild: boolean, path: number){
+  // we subtract one because this is a null node
+  // but we incremented one to the path that leads to it
   if(!root) return path - 1;
-  return Math.max(dfs(root.left, true, isLeftChild ? 1 : path + 1), dfs(root.right, false, isLeftChild ? path + 1 : 1));
+  
+  // if the current node is a left child
+  // then we reset the left path to 1 again
+  // because there's no zig zag here
+  // if it is not a left child, then we increment the 
+  // path by one
+  const newLeftPath = isLeftChild ? 1 : path + 1;
+  
+  // if the current node is a left child
+  // then we increment the path by one (because 
+  // we are going to the right now) but if it is a 
+  // right child, then we reset the path to one again
+  const newRightPath = isLeftChild ? path + 1 : 1;
+  return Math.max(dfs(root.left, true, newLeftPath), dfs(root.right, false, newRightPath));
 }
