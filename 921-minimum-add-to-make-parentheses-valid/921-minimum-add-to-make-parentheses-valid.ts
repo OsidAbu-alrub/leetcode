e4dbe76e0f3274a60eb68f9ba83b0b4f765b4275
Osid Abu-Alrub <o.abualrub20@gmail.com>
2022-09-22 19:53:00 +0300

    Time: 78 ms (83.33%), Space: 45.2 MB (12.50%) - LeetHub

--- 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.ts
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.ts
@@ -1,5 +1,11 @@
 function minAddToMakeValid(s: string): number {
-  const stack = [];;
+  // solve it as a normal valid parentheses question
+  // but when you find a closing parenthesis that 
+  // doesn't have an opening parentheses, then we 
+  // add the closing prentheses to the stack
+  // stack length is the number that we need to make 
+  // it valid
+  const stack = [];
   for(const char of s){
     if(char === "("){
       stack.push(char);
